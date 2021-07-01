ebd36743405e871ba6e5d5b1fedfd5913a970d0d
Emily <64155414+AnarkittyEmily@users.noreply.github.com>
2021-07-01 15:42:32 +0100

    Create calc.rs

---
+++ calc.rs
@@ -0,0 +1,14 @@
+pub fn add(first_number: i64, second_number: i64) -> i64 { 
+    return first_number + second_number; 
+}
+
+pub fn subtract(first_number: i64, second_number: i64) -> i64 { 
+    return first_number - second_number; 
+}
+
+pub fn multiply(first_number: i64, second_number: i64) -> i64 { 
+    return first_number * second_number; 
+}
+pub fn divide(first_number: i64, second_number: i64) -> i64 { 
+    return first_number / second_number; 
+}
